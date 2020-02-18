56267fb606991f1182466a567579acc3d97799e4
Bart Sakowski <bart.sakowski@gmail.com>
2020-02-18 04:04:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(array, name) {
-  array.push(element);
+  array.push(name);
   return(array)
 }
+
+function destructivelyPrependKitten(array, name) {
+  array.unshift(name);
+  return(array)
+}
+function destructivelyRemoveLastKitten(array) {
+  array.pop()
+  return(array)
+}
+
+function destructivelyRemoveFirstKitten(array) {
+  array.shift()
+  return(array)
+}
+
+function appendKitten(name)
