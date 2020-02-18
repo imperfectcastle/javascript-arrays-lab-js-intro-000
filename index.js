82eaa9b546aa720b36cef0435cda9e879ffa8e75
Bart Sakowski <bart.sakowski@gmail.com>
2020-02-18 03:56:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
-function destructivelyAppendKitten(name) {
-  return(name.push)
+function destructivelyAppendKitten(array, name) {
+  return(array.push(name))
 }
